a84c60d018b58302f0e9d55e0dbb6cad7431b754
kjbgkjlhkl <111585076+kjbgkjlhkl@users.noreply.github.com>
2022-08-19 00:51:20 -0600

    push_pipjsjjsjs

--- test_calc.py
+++ test_calc.py
@@ -4,14 +4,14 @@ import calc
 
 class testCalc(unittest.Testcase):
     def test_sum(self):
-        result= calc.sum(num_1=5, num_2=10)
-        self.assertequal(result,15)
+        result = calc.sum(num_1=5, num_2= 10)
+        self.assertEqual(result, 15)
   
     def test_divide(self):
-        result= calc.divide(num_1=10,num_2=2)
-        self.assertequal(result,5)
+        result = calc.divide(num_1=10,num_2= 2)
+        self.assertEqual(result, 5)
     
     def test_multiply(self):
-        result= calc.multiply(num_1=5,num_2=2)
-        self.assertequal(result,10)
-        
+        result = calc.multiply(num_1=5,num_2= 2)
+        self.assertEqual(result, 10)
+
